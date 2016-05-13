b6323c54ee2eedf013c130eaf02c9db2cea7c392
sudeepth <sudeepth>
2016-05-13 10:36:51 +0530

    *ngfor loop for applicant cards

--- app/content/applicants/applicant-list.component.js
+++ app/content/applicants/applicant-list.component.js
@@ -11,6 +11,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var ApplicantCardsComponent = (function () {
     function ApplicantCardsComponent() {
+        this.HEROES = [
+            { "id": 11, "name": "Mr. Nice", "city": "Phoenix", "state": "Arizona" },
+            { "id": 12, "name": "Narco", "city": "Phoenix", "state": "Arizona" },
+            { "id": 13, "name": "Bombasto", "city": "Phoenix", "state": "Arizona" },
+            { "id": 14, "name": "Celeritas", "city": "Phoenix", "state": "Arizona" },
+            { "id": 15, "name": "Magneta", "city": "Phoenix", "state": "Arizona" },
+            { "id": 16, "name": "RubberMan", "city": "Phoenix", "state": "Arizona" },
+            { "id": 17, "name": "Dynama", "city": "Phoenix", "state": "Arizona" },
+            { "id": 18, "name": "Dr IQ", "city": "Phoenix", "state": "Arizona" },
+            { "id": 19, "name": "Magma", "city": "Phoenix", "state": "Arizona" },
+            { "id": 20, "name": "Tornado", "city": "Phoenix", "state": "Arizona" }
+        ];
     }
     ApplicantCardsComponent = __decorate([
         core_1.Component({
--- app/content/applicants/applicant-list.component.ts
+++ app/content/applicants/applicant-list.component.ts
@@ -4,4 +4,18 @@ import {Component} from '@angular/core';
     selector: 'applicant-cards',
     templateUrl: 'app/content/applicants/applicant-list.component.html',
 })
-export class ApplicantCardsComponent { }
+export class ApplicantCardsComponent {
+
+	HEROES: any[] = [
+  { "id": 11, "name": "Mr. Nice", "city": "Phoenix", "state": "Arizona"},
+  { "id": 12, "name": "Narco", "city": "Phoenix", "state": "Arizona" },
+  { "id": 13, "name": "Bombasto", "city": "Phoenix", "state": "Arizona" },
+  { "id": 14, "name": "Celeritas", "city": "Phoenix", "state": "Arizona" },
+  { "id": 15, "name": "Magneta", "city": "Phoenix", "state": "Arizona" },
+  { "id": 16, "name": "RubberMan", "city": "Phoenix", "state": "Arizona" },
+  { "id": 17, "name": "Dynama", "city": "Phoenix", "state": "Arizona" },
+  { "id": 18, "name": "Dr IQ", "city": "Phoenix", "state": "Arizona" },
+  { "id": 19, "name": "Magma", "city": "Phoenix", "state": "Arizona" },
+  { "id": 20, "name": "Tornado", "city": "Phoenix", "state": "Arizona" }
+];
+ }
